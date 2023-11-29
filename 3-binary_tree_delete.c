49ab4e6593582f9329b5b7a7e8e5c2b0d53179c3
DjangoSpop <behodjango@gmail.com>
2023-11-29 20:58:44 +0200

    we can

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,21 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - free an entire tree using recursion it free the node
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
 
-
-
-void binary_tree_delete(binary_tree_t *tree) {
-    // Base case: if tree is NULL, do nothing
-    if (tree == NULL) {
-        return;
-    }
-
-    // Recursively delete the left subtree
-    binary_tree_delete(tree->left);
-
-    // Recursively delete the right subtree
-    binary_tree_delete(tree->right);
-
-    // Free the memory allocated for the root node
-    free(tree);
 }
